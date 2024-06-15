437a3cb3e86afa82c47a0a922fd9bc360c9d3b7b
tsuyuhazuki91 <161035282+tsuyuhazuki91@users.noreply.github.com>
2024-06-15 16:50:32 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>7章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+    $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($array);
+    ?>
+  </p>
+  
+</body>
+</html>
