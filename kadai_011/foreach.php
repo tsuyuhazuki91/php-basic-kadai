43c8f243b137c616aa24bfa47b137b2613d21301
tsuyuhazuki91 <161035282+tsuyuhazuki91@users.noreply.github.com>
2024-06-18 18:59:49 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>11章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+      $item_data = ['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道'];
+
+      foreach ($item_data as $key => $value) {
+        echo "{$key}:{$value}<br>";
+      }
+    ?>
+  </p>
+</body>
+</html>
